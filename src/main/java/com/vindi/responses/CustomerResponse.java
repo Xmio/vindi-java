e2ece5459bc5e31fb020d4998c03465276ce13b0
João Emilio Dambroz <joaoemiliod@gmail.com>
2018-11-12 16:53:37 -0200

    Including payment profile

--- src/main/java/com/vindi/responses/CustomerResponse.java
+++ src/main/java/com/vindi/responses/CustomerResponse.java
@@ -1,5 +1,7 @@
 package com.vindi.responses;
 
+import org.codehaus.jackson.annotate.JsonIgnoreProperties;
+
 import com.vindi.model.Customer;
 
 import lombok.AllArgsConstructor;
@@ -11,6 +13,7 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class CustomerResponse extends Customer {
 
 	private static final long serialVersionUID = 3177100649907218908L;
--- src/main/java/com/vindi/responses/PaymentProfileResponse.java
+++ src/main/java/com/vindi/responses/PaymentProfileResponse.java
@@ -1,5 +1,7 @@
 package com.vindi.responses;
 
+import org.codehaus.jackson.annotate.JsonIgnoreProperties;
+
 import com.vindi.model.PaymentProfile;
 
 import lombok.AllArgsConstructor;
@@ -11,6 +13,7 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class PaymentProfileResponse extends PaymentProfile {
 
 	private static final long serialVersionUID = 3177100649907218908L;
